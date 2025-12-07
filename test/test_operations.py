01ca375d3e811efcd6585526343ce7b915780685
sarthak-123465 <sarthak.gupta@hotmail.com>
2025-12-07 12:42:32 +0530

    new mathematical functionality

--- test/test_operations.py
+++ test/test_operations.py
@@ -1,6 +1,6 @@
 # Assuming this is your test file (e.g., tests/test_math.py)
 
-from src.math_operations import add, sub
+from src.math_operations import add, sub,prod
 
 def test_add():
     """
@@ -19,3 +19,8 @@ def test_sub():
     assert sub(1, -1) == 2
     assert sub(1,1) == 2
     # You can add more assertions here
+
+def test_prod():
+    assert prod(2,3) == 6
+    assert prod(1,0) == 0
+    assert prod(-1,3) == -3
